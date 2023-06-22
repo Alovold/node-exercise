5a5e54578e69ef993b8a1c596e1f335fb00178a2
Austin Lovold <austin@lovolds.com>
2023-06-22 14:31:23 -0500

    Completed exercise

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 // TODO: import router from users.route
 import userRouter from "./users.route.js"
-
+import employeeRouter from "./employees.routes.js";
 
 const router = express.Router();
 router.use("/users", userRouter)
 router.get("/test", (req, res) => {
   res.send("working");
 });
+router.use("/employees", employeeRouter);
 
 // TODO: use the imported router to handle all routes matching "/users"
 
